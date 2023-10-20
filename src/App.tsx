25cbcbcc48aaefdba03c69c2b4b985bca76a1c8c
George007361 <gera.illarionov@mail.ru>
2023-10-20 17:49:32 +0300

    add ts relative paths: @components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import styles from './App.module.scss';
 
 const App: React.FC = () => {
-  return <div>Hello app!</div>;
+  return <div className={styles.app}>Hello app!</div>;
 };
 
 export default App;
