2d6ceb2db69fab5e00ac076fafdc713dffc5b414
George007361 <gera.illarionov@mail.ru>
2023-10-29 23:45:54 +0300

    uikit: image viewer change

--- src/ui-kit/ImageViewer/ImageViewer.tsx
+++ src/ui-kit/ImageViewer/ImageViewer.tsx
@@ -1,85 +1,69 @@
 import React, { useCallback, useEffect, useState } from 'react';
+
+import styles from './ImageViewer.module.scss';
 import Container from '@ui-kit/Container/Container';
 import Button from '@ui-kit/Button/Button';
 import Image from '@ui-kit/Image/Image';
+import { UiComponentProps } from '@ui-kit/interfaces';
 
-import styles from './ImageViewer.module.scss';
-
-interface ImageViewerProps {
+interface ImageViewerProps extends UiComponentProps{
     src: string[];
 }
-const ImageViewer: React.FC<ImageViewerProps> = ({ src }) => {
+const ImageViewer: React.FC<ImageViewerProps> = ({ src, classes }) => {
     const [index, setIndex] = useState<number>(0);
-    const [loading, setLoading] = useState<boolean>(true);
-    const [rightArrow, setRightArrow] = useState(false);
-    const [leftArrow, setLeftArrow] = useState(false);
-
-    const handleLoad = useCallback(() => {
-        setLoading(false);
-    }, []);
-
-    const nextImage = useCallback(() => {
-        if (index < src.length - 1) {
-            setIndex(index + 1);
-        }
-    }, [index]);
-
-    const prevImage = useCallback(() => {
-        if (index > 0) {
-            setIndex(index - 1);
-        }
-    }, [index]);
+    const [leftArrow, setLeftArrow] = useState<boolean>(false);
+    const [rightArrow, setRightArrow] = useState<boolean>(false);
 
     useEffect(() => {
         setLeftArrow(index > 0 ? true : false);
         setRightArrow(index < src.length - 1 ? true : false);
-    }, [index]);
+    }, [src, index, setLeftArrow, setRightArrow]);
 
-    const handleKeydown = useCallback(
-        (e: KeyboardEvent) => {
-            const { code } = e;
-            if (code === 'ArrowLeft') {
-                prevImage();
-            }
-            if (code === 'ArrowRight') {
-                nextImage();
-            }
-        },
-        [prevImage, nextImage],
-    );
+    const prev = useCallback(() => {
+        if (index > 0) {
+            setIndex(index - 1);
+        }
+    }, [setIndex, index]);
+
+
+    const next = useCallback(() => {
+        if (index < src.length - 1) {
+            setIndex(index + 1);
+        }
+    }, [src, setIndex, index]);
+
+    const handleKeydown = useCallback((e: KeyboardEvent) => {
+        const { code } = e;
+        if (code === 'ArrowLeft') {
+            prev();
+        }
+        if (code === 'ArrowRight') {
+            next();
+        }
+    }, [prev, next]);
 
     useEffect(() => {
         document.addEventListener('keydown', handleKeydown);
-        return () => {
-            document.removeEventListener('keydown', handleKeydown);
-        };
-    });
+        return () => document.removeEventListener('keydown', handleKeydown);
+    }, [handleKeydown]);
 
     return (
         <Container
-            direction={'horizontal'}
-            classes={[styles.viewer].join(' ')}
-        >
+            direction='horizontal'
+            classes={[classes, styles.viewer].join(' ')}>
             <Button
-                disabled={loading || !leftArrow}
-                onClick={prevImage}
-            >
-                {'<'}
-            </Button>
-            {src.length > 0 && (
-                <Image
-                    src={src[index]}
-                    onLoad={handleLoad}
-                    classes={[styles.viewer__image].join(' ')}
-                />
-            )}
-            {!src.length && <div>No images</div>}
+                type='link'
+                classes={styles.navBtn}
+                disabled={!leftArrow}
+                onClick={prev} >{'<'}</Button>
+            <Image
+                classes={styles.image}
+                src={src[index]} />
             <Button
-                disabled={loading || !rightArrow}
-                onClick={nextImage}
-            >
-                {'>'}
-            </Button>
+                type='link'
+                classes={styles.navBtn}
+                disabled={!rightArrow}
+                onClick={next}>{'>'}</Button>
         </Container>
     );
 };
