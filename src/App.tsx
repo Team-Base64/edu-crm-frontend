87f656aafe50b9ac1e5d8402c329cd0cfa83cf63
George007361 <gera.illarionov@mail.ru>
2023-11-01 01:02:53 +0300

    tsconf hoc and pages pathes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import MainLayout from '@components/MainLayout/MainLayout';
-import RequireAuth from 'hoc/RequireAuth';
-import LandingPage from 'pages/Landing.page';
-import LoginPage from 'pages/Login.page';
-import NotFoundPage from 'pages/NotFound.page';
-import TestPage from 'pages/Test.page';
+import RequireAuth from '@hoc/RequireAuth';
+import LandingPage from '@pages/Landing.page';
+import LoginPage from '@pages/Login.page';
+import NotFoundPage from '@pages/NotFound.page';
+import TestPage from '@pages/Test.page';
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
