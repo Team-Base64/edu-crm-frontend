b00f78e72ae52648627c7b36990c75fc3447d004
George007361 <gera.illarionov@mail.ru>
2023-11-01 00:58:34 +0300

    sidebar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,29 +1,48 @@
-import { UiComponentProps } from "@ui-kit/interfaces";
+import { UiComponentProps } from '@ui-kit/interfaces';
 import styles from './Sidebar.module.scss';
-import Container from "@ui-kit/Container/Container";
-import SidebarButton from "@components/SidebarButton/SidebarButton";
+import Container from '@ui-kit/Container/Container';
+import SidebarButton from '@components/SidebarButton/SidebarButton';
 
-interface SidebarProps extends UiComponentProps {
-}
+interface SidebarProps extends UiComponentProps {}
 
-const Sidebar: React.FC<SidebarProps> = ({ }) => {
-
+const Sidebar: React.FC<SidebarProps> = () => {
     return (
-        <Container classes={styles.sidebar} direction="vertical">
-            <Container classes={styles.group} direction="vertical">
-                <SidebarButton isActive={false} title='test' iconName='homeLine' onClick={() => { }} />
-                <SidebarButton isActive={false} title='test' iconName='homeLine' onClick={() => { }} />
-                <SidebarButton isActive={false} title='test' iconName='homeLine' onClick={() => { }} />
-                <SidebarButton isActive={false} title='test' iconName='homeLine' onClick={() => { }} />
-                <SidebarButton isActive={false} title='test' iconName='homeLine' onClick={() => { }} />
-
+        <Container
+            classes={styles.sidebar}
+            direction="vertical"
+        >
+            <Container
+                classes={styles.group}
+                direction="vertical"
+            >
+                <SidebarButton
+                    title="tes23232t"
+                    iconName="homeLine"
+                    path="/test"
+                />
+                <SidebarButton
+                    title="test"
+                    iconName="homeLine"
+                    path="/test2"
+                />
+                <SidebarButton
+                    title="home"
+                    iconName="homeLine"
+                    path="/"
+                />
             </Container>
-            <Container classes={styles.group} direction="vertical">
-                <SidebarButton isActive={false} title='test' iconName='homeLine' onClick={() => { }} />
-
+            <Container
+                classes={styles.group}
+                direction="vertical"
+            >
+                <SidebarButton
+                    title="login"
+                    iconName="homeLine"
+                    path="/login"
+                />
             </Container>
         </Container>
     );
-}
+};
 
 export default Sidebar;
--- src/components/SidebarButton/SidebarButton.tsx
+++ src/components/SidebarButton/SidebarButton.tsx
@@ -1,27 +1,53 @@
-import Button, { ButtonSize } from "@ui-kit/Button/Button";
-import Icon, { IconName, IconSize } from "@ui-kit/Icon/Icon";
-import Text from "@ui-kit/Text/Text";
-import { UiComponentProps } from "@ui-kit/interfaces";
+import Button, { ButtonSize } from '@ui-kit/Button/Button';
+import Icon, { IconName, IconSize } from '@ui-kit/Icon/Icon';
+import Text from '@ui-kit/Text/Text';
 import styles from './SidebarItem.module.scss';
+import { NavLink } from 'react-router-dom';
 
-interface SidebarButtonProps extends UiComponentProps {
+interface SidebarButtonProps {
     iconName: IconName;
     title: string;
-    isActive: boolean;
+    path: string;
     btnSize?: ButtonSize;
     iconSize?: IconSize;
 }
 
-const SidebarButton: React.FC<SidebarButtonProps> = ({ btnSize = 's', iconSize = 'small', onClick, title, iconName, isActive }) => {
-
+const SidebarButton: React.FC<SidebarButtonProps> = ({
+    btnSize = 's',
+    iconSize = 'small',
+    title,
+    iconName,
+    path,
+}) => {
     return (
-        <li className={styles.item}>
-            <Button size={btnSize} classes={styles.button} type="link">
-                <Icon classes={styles.icon} size={iconSize} name={iconName} />
-                <Text classes={styles.title} type="h6" weight="bold">{title}</Text>
-            </Button>
+        <li className={styles.li}>
+            <NavLink
+                to={path}
+                className={({ isActive }) =>
+                    [styles.a, isActive ? styles.active : ''].join(' ')
+                }
+            >
+                <Button
+                    size={btnSize}
+                    classes={styles.button}
+                    type="link"
+                >
+                    <Icon
+                        classes={styles.icon}
+                        size={iconSize}
+                        name={iconName}
+                    />
+                    <Text
+                        classes={styles.title}
+                        type="h6"
+                        weight="bold"
+                    >
+                        {title}
+                    </Text>
+                </Button>
+            </NavLink>
         </li>
     );
-}
+};
 
 export default SidebarButton;
