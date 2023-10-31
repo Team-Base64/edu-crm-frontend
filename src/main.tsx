2eb7cba88a987380d62edcc59fc0d805ae765bb2
George007361 <gera.illarionov@mail.ru>
2023-10-31 23:21:08 +0300

    App base routing

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App.tsx';
 import './index.scss';
 import { store } from './app/store.ts';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 
 async function deferRender() {
     if (process.env.NODE_ENV !== 'mock') {
@@ -18,9 +19,11 @@ async function deferRender() {
 deferRender().then(() => {
     ReactDOM.createRoot(document.getElementById('root')!).render(
         <React.StrictMode>
-            <Provider store={store}>
-                <App />
-            </Provider>
+            <BrowserRouter>
+                <Provider store={store}>
+                    <App />
+                </Provider>
+            </BrowserRouter>
         </React.StrictMode>,
     );
 });
