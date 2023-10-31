c7c950c981a953313ddd9fc4b986eb261d650c9a
George007361 <gera.illarionov@mail.ru>
2023-10-31 23:20:15 +0300

    some pages

---
+++ src/pages/NotFound.page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFoundPage: React.FC = () => {
+    return (
+        <div>
+            <h1>404 PAGE</h1>
+            <p>IN PROGESS</p>
+        </div>
+    );
+};
+
+export default NotFoundPage;
