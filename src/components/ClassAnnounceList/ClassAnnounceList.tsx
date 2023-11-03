8c58929c1878290f256e108153ffb91a8a50f227
George007361 <gera.illarionov@mail.ru>
2023-11-03 21:55:44 +0300

    feed list component

---
+++ src/components/ClassAnnounceList/ClassAnnounceList.tsx
@@ -0,0 +1,48 @@
+import { UiComponentProps } from "@ui-kit/interfaces";
+import React, { useId } from "react";
+import styles from './ClassAnnounceList.module.scss';
+import { useGetClassAnnuncementsByIdQuery } from "app/features/api/class/classSlice";
+import ClassAnnounceCard from "@components/ClassAnnounceCard/ClassAnnounceCard";
+import Container from "@ui-kit/Container/Container";
+
+interface ClassAnnounceListProps extends UiComponentProps {
+    classId: string | number;
+}
+
+const ClassAnnounceList: React.FC<ClassAnnounceListProps> = ({ classId }) => {
+    const listId = useId();
+    const { data, isError, error } = useGetClassAnnuncementsByIdQuery({ id: classId });
+
+    if (!data?.announcements || isError) {
+        return (
+            <>
+                {isError && JSON.stringify(error)}
+                {!isError && 'Some error'}
+            </>
+        );
+    }
+
+    const list = data.announcements;
+    return (
+        <Container
+            direction='vertical'
+            layout='defaultBase'
+        >
+            {!list.length && 'EMPTY'}
+
+            {list.length && list.map(({ id, text, time }) => (
+                <React.Fragment key={`${listId}-${id}`}>
+                    <ClassAnnounceCard
+                        firstName="George"
+                        lastName="Illarionov"
+                        avatarSrc=""
+                        text={text}
+                        time={Number(time)}
+                    />
+                </React.Fragment>
+            ))}
+        </Container>
+    );
+}
+
+export default ClassAnnounceList;
