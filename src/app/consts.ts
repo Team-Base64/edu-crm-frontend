2ba7baf62056ce36b4020ea0ac09a2c50af6d49a
George007361 <gera.illarionov@mail.ru>
2023-11-03 18:26:40 +0300

    Class API features and urls

--- src/app/consts.ts
+++ src/app/consts.ts
@@ -2,6 +2,13 @@ export const apiPaths = {
     origin: 'http://127.0.0.1:8080',
     basePath: `http://127.0.0.1:8080/api/`,
     chats: 'chats',
+
+    classes: 'class',
+    class: (id : string | number) => `class/${id}`,
+    classNew: `class/new`,
+    classStundets: (id: string | number) => `class/${id}/students`,
+    classAnnouncements: (id: string | number) => `class/${id}/announcements`,
+    classAnnouncementsNew: (id: string | number) => `class/${id}/announcements`,
 };
 
 export const noop = () => {};
