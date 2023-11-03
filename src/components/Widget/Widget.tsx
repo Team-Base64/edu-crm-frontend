0508b1d1543a4f4af8b7c653a5d778e4a5b7ed91
George007361 <gera.illarionov@mail.ru>
2023-11-03 21:32:54 +0300

    simple widget component

---
+++ src/components/Widget/Widget.tsx
@@ -0,0 +1,33 @@
+import Container from "@ui-kit/Container/Container";
+import Text from "@ui-kit/Text/Text";
+
+import styles from './Widget.module.scss';
+import { UiComponentProps } from "@ui-kit/interfaces";
+
+interface WidgetProps extends UiComponentProps {
+    title: string;
+
+    nav?: JSX.Element;
+
+    footer?: JSX.Element;
+}
+
+const Widget: React.FC<WidgetProps> = ({ classes, title, nav, children, footer }) => {
+
+    return (
+        <>
+            <Container direction='vertical' layout='defaultBase' classes={[classes].join(' ')}>
+                <Container direction='horizontal' classes={styles.header}>
+                    <Text type="h" size={5} weight='bold'>
+                        {title}
+                    </Text>
+                    {nav}
+                </Container>
+                {children}
+                {footer}
+            </Container>
+        </>
+    );
+}
+
+export default Widget;
