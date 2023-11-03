c3456312a473422d9522dda02f92731c7ee0090c
George007361 <gera.illarionov@mail.ru>
2023-11-03 21:31:32 +0300

    class member list component

---
+++ src/components/ClassMemberList/ClassMemberList.tsx
@@ -0,0 +1,45 @@
+import ClassMemberItem from "@components/ClassMemberItem/ClassMemberItem";
+import { UiComponentProps } from "@ui-kit/interfaces";
+import { useGetClassUsersByIdQuery } from "app/features/api/class/classSlice";
+import React, { useId } from "react";
+import styles from './ClassMemberList.module.scss';
+
+interface ClassMemberListProps extends UiComponentProps {
+    classId: string | number;
+    limit?: number;
+}
+
+const ClassMemberList: React.FC<ClassMemberListProps> = ({ classId, limit }) => {
+    const listId = useId();
+    const { data, isError, error } = useGetClassUsersByIdQuery({ id: classId });
+
+    if (!data?.students || isError) {
+        return (
+            <>
+                {isError && JSON.stringify(error)}
+                {!isError && 'Some error'}
+            </>
+        );
+    }
+
+    const list = data.students;
+    return (
+        <>
+                {!list.length && 'EMPTY'}
+
+            {list.length && list.slice(0, limit).map(({ id, firstName, lastName, avatarSrc }) => (
+                <React.Fragment key={`${listId}-${id}`}>
+                    <ClassMemberItem
+                        id={id}
+                        firstName={firstName}
+                        lastName={lastName}
+                        avatarSrc={avatarSrc}
+                        role="Ученик"
+                    />
+                </React.Fragment>
+            ))}
+        </>
+    );
+}
+
+export default ClassMemberList;
