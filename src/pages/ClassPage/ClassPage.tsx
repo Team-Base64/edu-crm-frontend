d64ebe373a42861d6de19824e0bc48570dff535a
George007361 <gera.illarionov@mail.ru>
2023-11-05 01:55:51 +0300

    class page change

--- src/pages/ClassPage/ClassPage.tsx
+++ src/pages/ClassPage/ClassPage.tsx
@@ -1,9 +1,12 @@
 import ClassAnnounceWidget from '@components/ClassAnnounceWidget/ClassAnnounceWidget';
 import ClassHeader from '@components/ClassHeader/ClassHeader';
 import ClassMembersWidget from '@components/ClassMembersWidget/ClassMembersWidget';
+import ClassHomeworksWidget from '@components/HomeworkWidget/HomeworkWidget';
+import HomeworkSolutionWidget from '@components/HomeworkSolutionWidget/HomeworkSolutionWidget';
 import Container from '@ui-kit/Container/Container';
 import React from 'react';
 import { Navigate, useParams } from 'react-router-dom';
+import styles from './ClassPage.module.scss';
 
 const ClassPage: React.FC = () => {
     const params = useParams();
@@ -24,7 +27,24 @@ const ClassPage: React.FC = () => {
             gap="l"
         >
             <ClassHeader classId={id} />
-            <ClassMembersWidget classId={id} />
+            <Container
+                direction="horizontal"
+                gap="l"
+                classes={styles.widgets}
+            >
+                <ClassMembersWidget
+                    classId={id}
+                    classes={styles.widget}
+                />
+                <ClassHomeworksWidget
+                    classId={id}
+                    classes={styles.widget}
+                />
+                <HomeworkSolutionWidget
+                    classId={id}
+                    classes={styles.widget}
+                />
+            </Container>
             <ClassAnnounceWidget classId={id} />
         </Container>
     );
