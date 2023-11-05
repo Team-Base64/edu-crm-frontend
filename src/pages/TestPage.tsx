f06f78fe19d5a1323ea43ba087aa56799e8acdab
George007361 <gera.illarionov@mail.ru>
2023-11-05 14:27:34 +0300

    remove test page content

--- src/pages/TestPage.tsx
+++ src/pages/TestPage.tsx
@@ -1,4 +1,3 @@
-import AnnounceCreateField from '@components/AnnounceCreateField/AnnounceCreateField';
 import SendMessageArea from '@components/SendMessageArea/SendMessageArea';
 import React from 'react';
 
@@ -8,22 +7,6 @@ const TestPage: React.FC = () => {
             <h1>TEST PAGE</h1>
             <p>IN PROGESS</p>
 
-            <AnnounceCreateField avatarSrc="" />
-            <AnnounceCreateField avatarSrc="" />
-            <AnnounceCreateField avatarSrc="" />
-            <AnnounceCreateField avatarSrc="" />
-            <AnnounceCreateField avatarSrc="" />
-            <AnnounceCreateField avatarSrc="" />
-            <AnnounceCreateField avatarSrc="" />
-            <AnnounceCreateField avatarSrc="" />
-            <AnnounceCreateField avatarSrc="" />
-            <AnnounceCreateField avatarSrc="" />
-            <AnnounceCreateField avatarSrc="" />
-            <SendMessageArea
-                id="gg"
-                name="ggm"
-                onMessageSend={(text) => console.log(text)}
-            />
         </div>
     );
 };
