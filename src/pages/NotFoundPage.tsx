2646897974f34607bd45c2e1b88d175aa254afb4
George007361 <gera.illarionov@mail.ru>
2023-11-08 14:00:40 +0300

    fix styles

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,3 +1,5 @@
+import Container from '@ui-kit/Container/Container';
+import Text from '@ui-kit/Text/Text';
 import React from 'react';
 import { useLocation } from 'react-router-dom';
 
@@ -5,11 +7,15 @@ const NotFoundPage: React.FC = () => {
     const fromLocation = useLocation();
     const fromPath = fromLocation?.state?.from.pathname || 'NW';
     return (
-        <div>
-            <h1>404 PAGE</h1>
-            <p>IN PROGESS</p>
-            <p>From : {fromPath}</p>
-        </div>
+        <Container direction='vertical'>
+            <Text type='h' size={3} weight='bold'>
+                404 Not Found
+            </Text>
+            <Text type='p' size={1} weight='bold'>
+                Maybe page in progress...
+            </Text>
+            <div style={{height: '50px', width: '50px'}} className='test'/>
+        </Container>
     );
 };
 
