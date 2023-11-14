8fe65846a9d00a460998dd3cb15bfadd4c931490
George007361 <gera.illarionov@mail.ru>
2023-11-14 21:10:33 +0300

    Checkbox ui kit

---
+++ src/ui-kit/CheckBox/CheckBox.tsx
@@ -0,0 +1,43 @@
+import { UiComponentProps } from "@ui-kit/interfaces";
+import React from "react";
+import styles from './CheckBox.module.scss';
+
+const sizeStyle = {
+    s: styles.s,
+    m: styles.m,
+    l: styles.l,
+}
+
+type Size = keyof typeof sizeStyle;
+
+const typeStyle = {
+    primary: styles.primary,
+    secondary: styles.secondary,
+}
+
+type Type = keyof typeof typeStyle;
+
+interface CheckBoxProps extends UiComponentProps {
+    state: [boolean, React.Dispatch<React.SetStateAction<boolean>>];
+    size?: Size;
+    type?: Type;
+};
+
+
+const CheckBox: React.FC<CheckBoxProps> = ({ state, classes, size = 'm', type = 'primary' }) => {
+    const [checked, toggle] = state;
+    return (
+        <>
+            <input
+                className={[classes, typeStyle[type], sizeStyle[size], styles.checkbox].join(' ')}
+                type='checkbox'
+                defaultChecked={checked}
+                onChange={() => {
+                    toggle(s => !s);
+                }}
+            />
+        </>
+    );
+}
+
+export default CheckBox;
