2252fd7fbb8261c25e1b6846823e647c7d9104c6
George007361 <gera.illarionov@mail.ru>
2023-11-15 19:43:35 +0300

    add throttle and debounce

---
+++ src/ui-kit/_hooks/useThrottle.ts
@@ -0,0 +1,13 @@
+import { throttle } from "@ui-kit/_utils/throttle";
+import { useEffect } from "react";
+
+export const useThrottle = <A = unknown, R = void>(
+    fn: (args: A) => R,
+    ms: number
+): ((args: A) => Promise<R>) => {
+    const [throttledFunc, teardown] = throttle<A, R>(fn, ms);
+
+    useEffect(() => () => teardown(), []);
+
+    return throttledFunc;
+};
---
+++ src/ui-kit/_utils/throttle.ts
@@ -0,0 +1,24 @@
+export function throttle<A = unknown, R = void>(
+    fn: (args: A) => R,
+    ms: number
+): [(args: A) => Promise<R>, () => void] {
+    let timer: ReturnType<typeof setTimeout> | undefined = undefined;
+
+    const throttledFunc = (args: A): Promise<R> =>
+        new Promise((resolve) => {
+            if (timer) {
+                return;
+            }
+
+            timer = setTimeout(() => {
+                clearTimeout(timer);
+                timer = undefined;
+                console.log('thr');
+                resolve(fn(args));
+            }, ms);
+        });
+
+    const teardown = () => clearTimeout(timer);
+
+    return [throttledFunc, teardown];
+}
