c4b9f2c0a300da906fa19300ba2077a2e27596b8
George007361 <gera.illarionov@mail.ru>
2023-11-19 03:13:01 +0300

    rename tasks page in sidebar

--- src/components/Sidebar/SidebarConfig.ts
+++ src/components/Sidebar/SidebarConfig.ts
@@ -36,7 +36,7 @@ const config: SidebarConfig = [
         },
 
         {
-            title: 'Банк заданий',
+            title: 'Банк задач',
             iconName: 'pencilLine',
             path: AppRoutes.tasks,
         },
