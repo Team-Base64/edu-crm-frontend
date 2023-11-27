0b7b67edf72a572da86d6cb52db19be1c2540572
Aint <pinevichv@gmail.com>
2023-11-27 19:45:49 +0300

    [DEPLOY]

--- src/components/Messenger/Messenger.tsx
+++ src/components/Messenger/Messenger.tsx
@@ -6,8 +6,6 @@ import Container from '@ui-kit/Container/Container.tsx';
 import styles from './Messenger.module.scss';
 
 import { useGetLiveMessagesQuery } from '@app/features/chat/chatSlice.ts';
-import Text from '@ui-kit/Text/Text.tsx';
-import { unselectedId } from '@app/const/consts.ts';
 import Spinner from '@ui-kit/Spinner/Spinner.tsx';
 
 interface SendMessageAreaProps extends UiComponentProps {
