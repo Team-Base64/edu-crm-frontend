a1e3a32b0b951049c53119826d56a0ba4fde5622
George007361 <gera.illarionov@mail.ru>
2023-12-11 21:50:54 +0300

    update review after PUT

--- src/app/features/homeworkSolutionReview/reviewSlice.ts
+++ src/app/features/homeworkSolutionReview/reviewSlice.ts
@@ -15,9 +15,8 @@ export const reviewSlice = homeworkSolutionSlice.injectEndpoints({
                     body: JSON.stringify(payload),
                 };
             },
-            invalidatesTags: (_, __, arg) => {
-                return [{ type: 'Solutions', id: arg.solutionID }];
-            },
+            // TODO 
+            invalidatesTags: ['getHomeworkSolutions', 'getClassSolutions']
         }),
     }),
 });
