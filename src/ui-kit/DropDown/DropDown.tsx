7f3a1c03e5cddb9289ae3b6b1381ff57120c6593
George007361 <gera.illarionov@mail.ru>
2023-12-12 18:04:18 +0300

    refacor select component

--- src/ui-kit/DropDown/DropDown.tsx
+++ src/ui-kit/DropDown/DropDown.tsx
@@ -1,19 +1,20 @@
-import React, { useId } from 'react';
+import React, { useId, useMemo, useRef, useState } from 'react';
 import styles from './DropDown.module.scss';
 import InputBase, { InputBaseProps } from '@ui-kit/InputBase/InputBase';
 import basestyles from '../InputBase/InputBase.module.scss';
 import Icon from '@ui-kit/Icon/Icon';
 import Button from '@ui-kit/Button/Button';
+import Text from '@ui-kit/Text/Text';
+import Container from '@ui-kit/Container/Container';
 
 interface DropDownProps extends Omit<InputBaseProps, 'button'> {
-    //React.InputHTMLAttributes<HTMLSelectElement> {
     values: (number | string)[];
     placeholder?: number | string;
     initial?: number | string;
     name?: string;
     formatTitle?: (value: number | string) => number | string;
     disabled?: boolean;
-    onChange?: React.ChangeEventHandler<HTMLSelectElement>;
+    onChange?: React.ChangeEventHandler<HTMLInputElement>;
 }
 
 export const DropDown: React.FC<DropDownProps> = ({
@@ -27,50 +28,108 @@ export const DropDown: React.FC<DropDownProps> = ({
     onChange,
     ...rest
 }) => {
+    const [showList, setShowList] = useState<boolean>(false);
     const key = useId();
-    const options = [
-        <option
-            style={{ display: 'none' }}
-            key={`${key}-null`}
-            value={'initial-placeholder'}
-        >
-            {placeholder}
-        </option>,
-        ...values.map((value) => (
-            <option
-                key={`${key}-${value}`}
-                value={value}
-            >
-                {formatTitle ? formatTitle(value) : value}
-            </option>
-        )),
-    ];
-    const initialOption =
-        values.find((v) => v.toString() === initial?.toString()) ??
-        'initial-placeholder';
+    const fakeInput = useRef<HTMLInputElement>(null);
+    const [current, setCurrent] = useState<{ value: string; title: string }>(
+        initial
+            ? {
+                  value: initial.toString(),
+                  title: formatTitle
+                      ? formatTitle(initial).toString()
+                      : placeholder.toString(),
+              }
+            : {
+                  value: 'initial',
+                  title: placeholder.toString(),
+              },
+    );
+
+    const options = useMemo(
+        () =>
+            values.map((value) => {
+                const title = formatTitle ? formatTitle(value) : value;
+                return (
+                    <li
+                        className={styles.item}
+                        key={`${key}-${value}`}
+                        value={value}
+                        onClick={() => {
+                            setCurrent({
+                                value: value.toString(),
+                                title: title.toString(),
+                            });
+                            setShowList(false);
+
+                            if (!fakeInput.current) return;
+
+                            Object.getOwnPropertyDescriptor(
+                                window.HTMLInputElement.prototype,
+                                'value',
+                            )?.set?.call(fakeInput.current, value.toString());
+
+                            fakeInput.current.dispatchEvent(
+                                new Event('change', { bubbles: true }),
+                            );
+                        }}
+                    >
+                        <Text
+                            type="p"
+                            size={1}
+                        >
+                            {title}
+                        </Text>
+                    </li>
+                );
+            }),
+        [values, setCurrent, setShowList],
+    );
+
     return (
         <InputBase
             {...rest}
-            classes={[styles.select, classes, styles.cont].join(' ')}
+            classes={styles.select}
             button={
                 <Button
                     type="link"
-                    disabled={disabled}
                     classes={styles.btn}
+                    onClick={() => setShowList((st) => !st)}
                 >
-                    <Icon name="arrowDown" />
+                    <Icon
+                        name={showList ? 'arrowUp' : 'arrowDown'}
+                        classes={styles.icon}
+                    />
                 </Button>
             }
         >
-            <select
-                name={name}
-                className={[basestyles.input, styles.sel].join(' ')}
-                onChange={onChange}
-                defaultValue={initialOption}
-                disabled={disabled}
+            <Text
+                onClick={() => setShowList((st) => !st)}
+                type="p"
+                size={1}
+                classes={[basestyles.input, styles.current].join(' ')}
             >
-                {options}
-            </select>
+                {current.title}
+            </Text>
+            <input
+                style={{ display: 'none' }}
+                ref={fakeInput}
+                value={current.value}
+                onChange={onChange}
+            />
+            <ul
+                className={[
+                    styles.ul,
+                    showList ? styles.visible : styles.hidden,
+                ].join(' ')}
+            >
+                <Container
+                    direction="vertical"
+                    layout="defaultBase"
+                    classes={styles.container}
+                >
+                    {options}
+                </Container>
+            </ul>
         </InputBase>
     );
 };
